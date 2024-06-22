d83847efc233d2f7e8296d22030acf720c687595
R.Gokulprasath <79097212+Gokulprasath2002@users.noreply.github.com>
2024-06-22 14:48:52 +0530

    Update awsDocumentServices.py

--- services/awsDocumentServices.py
+++ services/awsDocumentServices.py
@@ -1,16 +1,15 @@
 from main import ExtractStrategyWrapper,LLM
-import os
+import os,re,json
 from textractor import Textractor
 from textractor.data.constants import TextractFeatures
 import pypdfium2 as pdfium
-from typing import List
-from openai import OpenAI
-from configparser import ConfigParser
+from typing import List,Dict
+import google.generativeai as genai
 
 class AwsExtractStrategy(ExtractStrategyWrapper):
 
     def __init__(self):
-        os.environ['AWS_CONFIG_FILE'] = 'C:/Users/ASUS/MyWorks/Projects/BillExtraction/config.ini'
+        os.environ['AWS_CONFIG_FILE'] = 'C:/Users\ASUS\MyWorks\Projects\GSTExtract\config\config.INI'
         self.extractor = Textractor(profile_name="default")
 
     def contains_pdf(self, file_string: str) -> bool:
@@ -50,7 +49,7 @@ class AwsExtractStrategy(ExtractStrategyWrapper):
                 features=TextractFeatures.LAYOUT
             )
             output = document.lines 
-
+            
             return output
         
         else:
@@ -62,30 +61,100 @@ class AwsExtractStrategy(ExtractStrategyWrapper):
 
 
 class awsLLM(LLM):
-    def __init__(self, data: List):
-        self.data = data
-        self.model = "gpt-3.5-turbo"
+    def __init__(self, config_type:str):
+        super().__init__(config_type)
 
-        super().__init__(self.model,self.data)
+        self.api_key = self.config['api_key']
+        genai.configure(api_key=self.config['api_key'])
 
-        self.client = OpenAI(api_key="<api-key>")
+        self.model = self.config['model']
+        self.temperature = self.config['temperature']
+        self.top_p = self.config['top_p']
+        self.top_k = self.config['top_k']
+        self.max_output_tokens = self.config['max_output_tokens']
+        self.response_mime_type = self.config['response_mime_type']
+
+        self.generation_config = {
+                                "temperature": float(self.temperature),
+                                "top_p": int(self.top_p),
+                                "top_k": int(self.top_k),
+                                "max_output_tokens": int(self.max_output_tokens),
+                                "response_mime_type": str(self.response_mime_type),
+                                }
         
-    def getLLMResponse(self):
+    def constructDictResponse(self,response:str):
+        try:
+            try:
+                response = response.strip("```JSON\n```")
+            except Exception as e:
+                pass
+
+            pattern = r'"([^"]+)"\s*:\s*"([^"]*)"|(\w+)\s*:\s*(null)'
+            matches = re.findall(pattern, response)
+            result = {}
+            for match in matches:
+                if match[0] and match[1]:
+                    result[match[0]] = match[1]
+                elif match[2]:
+                    result[match[2]] = None
+
+            result = json.dumps(result, indent=2) 
+            return result
         
-        completion = self.client.chat.completions.create(
-        model="gpt-3.5-turbo",
+        except Exception as e:
+            print("Error parsing the response, Returning the raw text response back")
+            return response
+        
+    def getLLMResponse(self,data:List[str])->str:
 
-        messages=[
-            {"role": "system", "content": "You are a good data extractor. Extract the data from the raw as mentioned. Do not return any unwanted text"},
-            {"role": "user", "content": self.prompt}
-                ]
-        )
+        data = f"""{data}"""
+        self.prompt=f"""The below is text extracted from the bill/invoice form image using AWS/AZURE. 
+                        Understand the data and extract the field which I have mentioned below
+                        Data: {data}
 
-        return completion.choices[0].message
+                        Output Fields:
+                            Invoice no
+                            GSTIN
+                            Name
+                            M/s
+                            Party's GSTIN
+                            E-way Bill No
+                            State
+                            State code
+                            Date
+                            Description
+                            HSN
+                            Unit
+                            Rate
+                            CGST
+                            SGST
+                            Igst
+                            Total
+                            Grand total
+
+                        NOTE: THE OUTPUT SHOULD BE IN JSON FORMAT. 
+                        ONLY INCLUDE THE FILEDS WHICH I HAVE MENTIONED IN THE OUTPUT FIELD. 
+                        DO NOT RETURN ANY EXTRA TEXT. KEEP IT STRAIGHT TO THE REQUEST."""
+
+        model = genai.GenerativeModel(
+                                        model_name = self.model,
+                                        generation_config = self.generation_config,
+                                    )
+        
+        chat_session = model.start_chat(
+                                            history=[]
+                                       )
+        response = chat_session.send_message(str(self.prompt))
+
+        response = self.constructDictResponse(response.text)
+
+        return response
     
 
 aws = AwsExtractStrategy()
-extracted_data = aws.extract("D:/Ticket.pdf")
+extracted_data = aws.extract("C:/Users\ASUS\MyWorks\Projects\GSTExtract\storage\IMG-20240601-WA0024.jpg")
 
-aws_gpt  = awsLLM(extracted_data)
-transformed_data = aws_gpt.getLLMResponse()
+aws_gpt  = awsLLM('GEMINI')
+
+transformed_data = aws_gpt.getLLMResponse(extracted_data)
+print(transformed_data)
