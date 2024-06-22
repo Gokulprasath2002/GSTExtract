18dcafad821181076ca809cd75ace1e4dd865e02
R.Gokulprasath <79097212+Gokulprasath2002@users.noreply.github.com>
2024-06-22 14:49:11 +0530

    Update main.py

--- services/main.py
+++ services/main.py
@@ -1,6 +1,7 @@
-from typing import List
+from typing import List,Dict
 from abc import ABC, abstractmethod
 from configparser import ConfigParser
+import json
 
 class ExtractStrategyWrapper(ABC):
     def __init__(self,config_type:str) -> None:
@@ -19,40 +20,13 @@ class ExtractStrategyWrapper(ABC):
 
 class LLM():
 
-    def __init__(self,model:str,data:List,temp:float=0.3) ->None:
-        self.prompt=f"""The below is text extracted from the bill/invoice form image using AWS/AZURE. 
-                        Understand the data and extract the field which I have mentioned below
-                        Data: {data}
-
-                        Output Fields:
-                            Invoice no
-                            GSTIN
-                            Name
-                            M/s
-                            Party's GSTIN
-                            E-way Bill No
-                            State
-                            State code
-                            Date
-                            Description
-                            HSN
-                            Unit
-                            Rate
-                            CGST
-                            SGST
-                            Igst
-                            Total
-                            Grand total
-
-                        NOTE: THE OUTPUT SHOULD BE IN JSON FORMAT. 
-                        ONLY INCLUDE THE FILEDS WHICH I HAVE MENTIONED IN THE OUTPUT FIELD. 
-                        DO NOT RETURN ANY EXTRA TEXT. KEEP IT STRAIGHT TO THE REQUEST."""
-        self.model=model
-        self.temp=temp
-        
-
+    def __init__(self, config_type:str) ->None:
+        config=ConfigParser()
+        config.read("config\config.INI")
+        self.config=config[config_type]
+                
     def getLLMResponse(self,extractData:List[dict]):
         pass
 
-
-
+    def constructDictResponse(self,response:str):
+        pass
